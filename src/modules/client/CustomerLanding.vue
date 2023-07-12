6729beee940d180b4a1daddf53fb7142a6c351ff
Rafashhe <cwb.rafaella@gmail.com>
2023-07-12 00:04:21 -0300

    cria landingPage para o comprador e configura função getDoc para puxar os appointments do db

---
+++ src/modules/client/CustomerLanding.vue
@@ -0,0 +1,40 @@
+<script>
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../../firebase.config.js";
+
+export default {
+  data() {
+    return {
+      appointments: [],
+    };
+  },
+  async mounted() {
+    await this.getAppointment();
+  },
+  methods: {
+    async getAppointment() {
+      const querySnapshot = await getDocs(collection(db, "customers"));
+      this.appointments = querySnapshot.docs.map((doc) => doc.data());
+    },
+  },
+};
+</script>
+
+<template>
+  <v-container class="bg-grey w-75">
+    <div class="d-flex align-center justify-end">
+       Clque para agendar visita <v-icon>mdi-arrow-right</v-icon>
+        <v-btn icon="mdi-plus" size="x-large" @click="$router.push({ name: 'booking' });"></v-btn>
+    </div>
+    <v-card class="mx-auto" max-width="300">
+      <v-list>
+        <v-list-item v-for="appointment in appointments" :key="appointment.id">
+          <v-list-item>{{ appointment.Property }}</v-list-item>
+          <v-list-item>{{ appointment.Realtor }}</v-list-item>
+          <v-list-item>{{ appointment.Time }}</v-list-item>
+          <v-list-item>{{ appointment.Date }}</v-list-item>
+        </v-list-item>
+      </v-list>
+    </v-card>
+  </v-container>
+</template>

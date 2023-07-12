6f6e9e1dfb9be3ee6b4681657051e56165fe2871
Rafashhe <cwb.rafaella@gmail.com>
2023-07-12 12:14:17 -0300

    troca nome do componente e transforma o modal em um modal de confirmação de informações

---
+++ src/modules/customer/components/ConfirmationModal.vue
@@ -0,0 +1,41 @@
+<script>
+export default {
+  props: {
+    modalTitle: String,
+    name: String,
+    person: String,
+    date: String,
+    time: String,
+    open: Boolean,
+  },
+  emits: ["close-modal", "submit"],
+  methods: {
+    closeModal() {
+      this.$emit("close-modal");
+    },
+    submitInfo() {
+      this.$emit("submit");
+      this.closeModal();
+    },
+  },
+};
+</script>
+
+
+<template>
+  <v-dialog :model-value="open" @click:outside="closeModal" width="900">
+    <v-card>
+      <v-card-title>{{ modalTitle }}</v-card-title>
+      <v-card-text>
+        <p>Lote: {{ name }}</p>
+          <p>Corretor(a): {{ person }}</p>
+          <p>Horário: {{ date }} - {{ time }}</p>
+          <span><strong>Duração da visita: </strong>1 hora</span>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn @click="submitInfo">Confirmar</v-btn>
+        <v-btn @click="closeModal">Cancelar</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>

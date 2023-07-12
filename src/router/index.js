37f747d25c9e9b4243cd7e818e2861bb7e90bcbc
Rafashhe <cwb.rafaella@gmail.com>
2023-07-12 00:02:19 -0300

    cria rotas temporárias

--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,21 @@ const routes = [
         // which is lazy-loaded when the route is visited.
         component: () => import(/* webpackChunkName: "home" */ '@/views/Home.vue'),
       },
+            {
+        path: '/user',
+        name: 'Buyer',
+        component: () => import('@/modules/client/BuyerView.vue'),
+      },
+      {
+        path: '/customer',
+        name: 'customerLanding',
+        component: () => import('@/modules/client/CustomerLanding.vue'),
+      },
+      {
+        path: '/booking',
+        name: 'booking',
+        component: () => import('@/modules/client/CustomerBooking.vue'),
+      },
     ],
   },
 ]

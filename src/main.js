fd5c194c43f938480f72824a490f56ed0be0b631
Rafashhe <cwb.rafaella@gmail.com>
2023-07-12 00:01:18 -0300

    instala bibliotecas de calendário

--- src/main.js
+++ src/main.js
@@ -13,8 +13,12 @@ import { createApp } from 'vue'
 // Plugins
 import { registerPlugins } from '@/plugins'
 
+import VCalendar from 'v-calendar';
+import 'v-calendar/style.css';
+
 const app = createApp(App)
 
 registerPlugins(app)
+app.use(VCalendar, {})
 
 app.mount('#app')

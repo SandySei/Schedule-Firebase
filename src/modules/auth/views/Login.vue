120af11a0e55981e1b79e83f33ad12473ff10539
Sandy <sandyalves8@hotmail.com>
2023-07-13 11:05:39 -0300

    Adição de Snackbar, div e alteraçoes de layout

--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -23,36 +23,55 @@ async function handleSignIn() {
   }
 
   if (result.role == "customer") {
+    emit("snackbar", "Login efetuado com sucesso!");
     router.push("/customer");
   } else {
+    emit("snackbar", "Login efetuado com sucesso!");
     router.push("/realtor");
   }
 }
 </script>
 
 <template>
-  <main>
-    <v-text-field v-model="email" label="E-mail"></v-text-field>
+  <v-container class="d-flex justify-center flex-column align-self-center ">
+    <div class=" animate__animated animate__backInDown">
+      <v-text-field
+        v-model="email"
+        variant="underlined"
+        clearable
+        label="E-mail"
+      ></v-text-field>
 
-    <v-text-field
-      v-model="password"
-      label="Senha"
-      :type="showPassword ? 'text' : 'password'"
-    ></v-text-field>
+      <v-text-field
+        v-model="password"
+        variant="underlined"
+        clearable
+        label="Senha"
+        :type="showPassword ? 'text' : 'password'"
+      ></v-text-field>
 
-    <v-checkbox
-      v-model="showPassword"
-      label="Ver senha"
-      color="grey"
-    ></v-checkbox>
+      <v-checkbox
+        v-model="showPassword"
+        label="Ver senha"
+        color="grey"
+      ></v-checkbox>
 
-    <v-btn
-      color="grey-darken-2"
-      type="submit"
-      @click="handleSignIn"
-      block
-      class="mt-2"
-      >Entrar</v-btn
-    >
-  </main>
+      <v-btn
+        color="grey-darken-2"
+        type="submit"
+        @click="handleSignIn"
+        block
+        class="mt-2"
+        >Entrar</v-btn
+      >
+      <p class="pt-6 pl-1 text-grey-darken-1">
+        Cadastre-se
+        <router-link
+          class="text-blue-accent-4 text-decoration-none"
+          to="/register"
+          >aqui!</router-link
+        >
+      </p>
+    </div>
+  </v-container>
 </template>

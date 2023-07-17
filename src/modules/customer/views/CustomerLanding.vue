cc8c1aacb4017c6ddba47e26b77bf98e4de6c659
Sandy <sandyalves8@hotmail.com>
2023-07-17 14:19:04 -0300

    Adição do menu, ordenação por data, e mudança de cor caso esteja no dia ou já tenha passado o dia

--- src/modules/customer/views/CustomerLanding.vue
+++ src/modules/customer/views/CustomerLanding.vue
@@ -1,6 +1,7 @@
 <script>
 import { getDocs, collection, getDoc, doc } from "firebase/firestore";
 import { db } from "../../../../firebase.config";
+import Menu from "@/modules/customer/components/Menu.vue";
 
 export default {
   data() {
@@ -10,6 +11,9 @@ export default {
   },
   async mounted() {
     await this.getAppointment();
+  },
+  components: {
+    Menu,
   },
   methods: {
     async getAppointment() {
@@ -25,7 +29,14 @@ export default {
           return data;
         })
       );
-      console.log(this.appointments);
+      this.sortAppointments(); // Ordena os agendamentos por data mais próxima
+    },
+    sortAppointments() {
+      this.appointments.sort((a, b) => {
+        const dateA = new Date(a.Date);
+        const dateB = new Date(b.Date);
+        return dateA - dateB;
+      });
     },
     getFormatDate(datetime) {
       if (!datetime) return "";
@@ -34,39 +45,99 @@ export default {
         date.getUTCMonth() + 1
       }/${date.getUTCFullYear()}`;
     },
+    isPastDate(date) {
+      const currentDate = new Date();
+      const appointmentDate = new Date(date);
+      return appointmentDate < currentDate;
+    },
   },
 };
 </script>
 
 <template>
-  <v-container class="bg-grey w-75">
-    <div class="d-flex align-center justify-end">
-      Clque para agendar visita <v-icon>mdi-arrow-right</v-icon>
-      <v-btn
-        icon="mdi-plus"
-        size="x-large"
+  <Menu class="mb-8 elevation-3"></Menu>
+
+  <v-container class="bg-grey-lighten-3 mb-8 elevation-2 w-75">
+    <div class="d-flex flex-row align-center justify-center w-100">
+      <div class="w-25"></div>
+      <h2 class="text-center w-50">Seus agendamentos:</h2>
+      <div
+        class="btn-container d-flex align-end justify-end mr-2 mt-2 w-25"
         @click="$router.push({ name: 'booking' })"
-      ></v-btn>
+      >
+        <v-btn
+          class="rounded-pill"
+          expand-on-hover
+          variant="flat"
+          @click="deleteAppointment(appointment.id)"
+        >
+          <p class="text text-body-2 text-grey-darken-2">
+            Clique para agendar visita
+          </p>
+          <span
+            ><v-icon size="x-large" color="grey-darken-2"
+              >mdi-plus</v-icon
+            ></span
+          >
+        </v-btn>
+      </div>
     </div>
-    <h1 class="text-center">Confira seus agendamentos</h1>
-    <div class="d-flex align-center justify-center mt-16 mb-5">
+
+    <div class="d-flex align-center flex-column justify-center mt-8 w-100">
       <v-card
         v-for="appointment in appointments"
         :key="appointment.id"
-        class="mx-auto"
-        max-width="300"
+        :class="{ 'past-date': isPastDate(appointment.Date)}"
+        class="w-75 mb-5 elevation-0 rounded-0"
       >
-        <v-list>
-          <v-list-item>
-            <v-list-item>{{ appointment.Property }}</v-list-item>
-            <v-list-item>{{ appointment.RealtorName }}</v-list-item>
-            <v-list-item
-              >{{ getFormatDate(appointment.Date) }} -
-              {{ appointment.Time }}</v-list-item
-            >
-          </v-list-item>
-        </v-list>
+        <v-card-title>{{ appointment.Property.landName }}</v-card-title>
+        <v-card-subtitle>{{
+          appointment.Property.landDescription
+        }}</v-card-subtitle>
+
+        <v-card-text
+          ><strong >Corretor:</strong>
+          {{ appointment.RealtorName }} <br />
+          <strong >Agendado para dia</strong>
+          {{ getFormatDate(appointment.Date) }}
+          <strong >às</strong>
+          {{ appointment.Time }}
+        </v-card-text>
       </v-card>
     </div>
   </v-container>
 </template>
+
+<style scoped>
+.box-glass {
+  background: rgba(212, 212, 212, 0.25);
+  box-shadow: 0 4px 10px 0 rgba(31, 38, 135, 0.37);
+  backdrop-filter: blur(4px);
+  -webkit-backdrop-filter: blur(4px);
+  border: 1px solid rgba(255, 255, 255, 0.18);
+}
+.rounded-pill {
+  height: 60px;
+}
+.btn-container:hover {
+  transition: 1s;
+}
+.text {
+  transition: 1s;
+  width: 0px;
+  overflow: hidden;
+}
+.btn-container:hover .text {
+  display: flex;
+  width: 180px;
+  align-content: center;
+}
+
+.past-date {
+    background-color:#B71C1C;
+}
+
+h2 {
+  font-size: 2rem;
+}
+</style>
